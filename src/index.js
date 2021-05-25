caffdd850a73f44578fd87af86db36cda6211a20
kyleli <hli162@uottawa.ca>
2021-05-25 15:13:25 -0400

    Deploy react app to github
    - Reference (Tutorial Part 10): https://ibaslogic.com/deploying-react-app-to-github-pages/

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BrowserRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 
 // Component file
 import TodoContainer from "./functionBased/components/TodoContainer";
@@ -11,7 +12,7 @@ import "./functionBased/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
